0a9494e635f8bb3e81378bb8c22933d2d31a8f8f
antonio mello babo <antoniomelloaviao@gmail.com>
2021-04-30 18:13:02 -0300

    refactor: update Account Repository interface

--- app/domain/account/repository.go
+++ app/domain/account/repository.go
@@ -1,15 +1,13 @@
 package account
 
-import "database/sql/driver"
-
 // Manage the persistence related to account entity
 type Repository interface {
 	Store(*Account) error
-	GetBalance(Account) error
-	ExistsByCPF(Account) error
-	UpdateBalance(Account) error
+	GetBalance(Account) (int, error)
+	ExistsByCPF(*Account) (bool, error)
+	UpdateBalance(Account, int) error
 	GetByCPF(string) (Account, error)
 	ShowAll() ([]Account, error)
-	GeneratedID() string
-	Transaction(driver.Tx) Repository
+	GenerateID() string
+	// Transaction(driver.Tx) Repository
 }
