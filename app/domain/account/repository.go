17bbe6d3b9e711ccb7dd2f0c610703a1e61a0b8d
MelloTonio <antoniombabo@uol.com.br>
2021-04-30 19:50:58 -0400

    refactor: pass account by reference on updateBalance

--- app/domain/account/repository.go
+++ app/domain/account/repository.go
@@ -5,7 +5,7 @@ type Repository interface {
 	Store(*Account) error
 	GetBalance(Account) (int, error)
 	ExistsByCPF(*Account) (bool, error)
-	UpdateBalance(Account, int) error
+	UpdateBalance(*Account, int) error
 	GetById(string) (Account, error)
 	GetByCPF(string) (Account, error)
 	ShowAll() ([]Account, error)
