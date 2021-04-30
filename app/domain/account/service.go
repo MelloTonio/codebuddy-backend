5ebd183411f25a235ad98547831bdf3982cc92af
antonio mello babo <antoniomelloaviao@gmail.com>
2021-04-30 18:50:36 -0300

    feat: ?

--- app/domain/account/service.go
+++ app/domain/account/service.go
@@ -4,6 +4,7 @@ package account
 When a significant process or transformation in the domain is not a natural responsibility
 of an ENTITY or VALUE OBJECT, add an operation to the model as standalone interface declared as a SERVICE.
 */
+*/
 
 type Service interface {
 	CreateAccount(*Account) error
