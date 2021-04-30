328dd98301094ee949a1cf54d9fca03f4a006698
antonio mello babo <antoniomelloaviao@gmail.com>
2021-04-30 18:13:42 -0300

    feat: add new errors

--- app/domain/errors/errors.go
+++ app/domain/errors/errors.go
@@ -7,8 +7,9 @@ import (
 
 // Domain errors.
 var (
-	Domain_Err          = errors.New("Domain Error")
-	EmptyAccountID_Err  = fmt.Errorf("%w: %v", Domain_Err, "Id cannot be Empty")
-	AccountNotFound_Err = fmt.Errorf("%w: %v", Domain_Err, "Account Not Found")
-	EmptyCPF_Err        = fmt.Errorf("%w: %v", Domain_Err, "CPF cannot be empty")
+	Domain_Err               = errors.New("Domain Error")
+	EmptyAccountID_Err       = fmt.Errorf("%w: %v", Domain_Err, "Id cannot be Empty")
+	AccountNotFound_Err      = fmt.Errorf("%w: %v", Domain_Err, "Account Not Found")
+	EmptyCPF_Err             = fmt.Errorf("%w: %v", Domain_Err, "CPF cannot be empty")
+	AccountAlreadyExists_Err = fmt.Errorf("%w: %v", Domain_Err, "Account already exists")
 )
