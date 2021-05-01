bfd363b98bbd43e54097b7ac31adc662a714a29c
antonio mello babo <antoniomelloaviao@gmail.com>
2021-05-01 18:22:53 -0300

    feat: create account validation

--- app/domain/account/validation.go
+++ app/domain/account/validation.go
@@ -1,9 +1,43 @@
 package account
 
+import (
+	"github.com/sirupsen/logrus"
+)
+
 type Validation interface {
-	Validate(Account) error
+	Validate() error
 }
 
 type InputCreationValidation interface {
 	Validate(InputValue)
 }
+
+type ValidationStruct struct {
+	account *Account
+	log     *logrus.Entry
+}
+
+/*func NewAccountValidation(acc *Account) Validation {
+	return &ValidationStruct{
+		account: acc,
+		log:     logrus.NewEntry(logrus.New()).WithField("source", "NewAccountValidation"),
+	}
+}
+
+func (vldStruct *ValidationStruct) Validate() error {
+	if vldStruct.account.Id == "" {
+
+		err := errors.EmptyAccountID_Err
+		vldStruct.log.WithError(err).Error("Empty Account Id")
+		return err
+	}
+
+	vldStruct.account.Cpf == "" {
+		err := errors.EmptyAccountCPF_Err
+		vldStruct.log.WithError(err).Error("Empty Account CPF")
+		return err
+	}
+
+	return nil
+}
+*/
