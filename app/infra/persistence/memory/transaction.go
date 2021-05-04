ba60f7e55bec62f5caa00f7201f1584d7ac1d709
antonio mello babo <antoniomelloaviao@gmail.com>
2021-05-04 18:23:27 -0300

    feat: add transaction'

---
+++ app/infra/persistence/memory/transaction.go
@@ -0,0 +1,27 @@
+package mem
+
+import (
+	"database/sql/driver"
+
+	"github.com/sirupsen/logrus"
+)
+
+func NewRepositoryTransaction() *MemRepositoryTrx {
+	return &MemRepositoryTrx{log: &logrus.Logger{}}
+}
+
+type MemRepositoryTrx struct {
+	log *logrus.Logger
+}
+
+func (MemRepositoryTrx) Commit() error {
+	return nil
+}
+
+func (MemRepositoryTrx) Rollback() error {
+	return nil
+}
+
+func (rr *MemRepositoryTrx) Tx() driver.Tx {
+	return &MemRepositoryTrx{}
+}
