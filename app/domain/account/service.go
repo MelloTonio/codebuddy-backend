e5671be27d12e9c777bbea9b85a9345072f603b0
antonio mello babo <antoniomelloaviao@gmail.com>
2021-05-04 18:18:15 -0300

    refactor: bug

--- app/domain/account/service.go
+++ app/domain/account/service.go
@@ -1,14 +1,13 @@
 package account
 
-/*
-When a significant process or transformation in the domain is not a natural responsibility
-of an ENTITY or VALUE OBJECT, add an operation to the model as standalone interface declared as a SERVICE.
-*/
-
 type Service interface {
 	CreateAccount(*Account) error
-	GetBalance(Account) (BalanceValue, error)
 	UpdateBalance(Account) error
 	GetAccount(string) (Account, error)
 	ShowAccounts() ([]Account, error)
 }
+
+/*
+When a significant process or transformation in the domain is not a natural responsibility
+of an ENTITY or VALUE OBJECT, add an operation to the model as standalone interface declared as a SERVICE.
+*/
