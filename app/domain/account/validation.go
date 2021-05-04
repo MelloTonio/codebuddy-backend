d0c655f1f1f674f6b78e736e5992dce57271c474
antonio mello babo <antoniomelloaviao@gmail.com>
2021-05-04 18:18:40 -0300

    refactor: remove validation comment

--- app/domain/account/validation.go
+++ app/domain/account/validation.go
@@ -16,28 +16,3 @@ type ValidationStruct struct {
 	account *Account
 	log     *logrus.Entry
 }
-
-/*func NewAccountValidation(acc *Account) Validation {
-	return &ValidationStruct{
-		account: acc,
-		log:     logrus.NewEntry(logrus.New()).WithField("source", "NewAccountValidation"),
-	}
-}
-
-func (vldStruct *ValidationStruct) Validate() error {
-	if vldStruct.account.Id == "" {
-
-		err := errors.EmptyAccountID_Err
-		vldStruct.log.WithError(err).Error("Empty Account Id")
-		return err
-	}
-
-	vldStruct.account.Cpf == "" {
-		err := errors.EmptyAccountCPF_Err
-		vldStruct.log.WithError(err).Error("Empty Account CPF")
-		return err
-	}
-
-	return nil
-}
-*/
