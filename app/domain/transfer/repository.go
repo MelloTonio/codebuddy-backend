e05eea99232881e5f0d0b3fc2292152df5caa6f8
antonio mello babo <antoniomelloaviao@gmail.com>
2021-05-04 18:21:27 -0300

    feat: add transaction in transfer repository

--- app/domain/transfer/repository.go
+++ app/domain/transfer/repository.go
@@ -1,9 +1,11 @@
 package transfer
 
+import "database/sql/driver"
+
 type Repository interface {
 	Store(*Transfer) error
 	GetById(string) (Transfer, error)
 	ListByAccId(string) ([]Transfer, error)
 	GenerateId() string
-	//Transaction(driver.Tx) Repository
+	Transaction(driver.Tx) Repository
 }
