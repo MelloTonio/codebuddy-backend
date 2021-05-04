3c31a1cc78acc6f4555fdf7136370bb02449a9bb
antonio mello babo <antoniomelloaviao@gmail.com>
2021-05-04 18:18:03 -0300

    refactor: add transaction in account repository

--- app/domain/account/repository.go
+++ app/domain/account/repository.go
@@ -1,14 +1,16 @@
 package account
 
+import "database/sql/driver"
+
 // Manage the persistence related to account entity
 type Repository interface {
 	Store(*Account) error
 	GetBalance(Account) (int, error)
 	ExistsByCPF(*Account) (bool, error)
-	UpdateBalance(*Account, int) error
+	UpdateBalance(*Account) error
 	GetById(string) (Account, error)
 	GetByCPF(string) (Account, error)
 	ShowAll() ([]Account, error)
 	GenerateID() string
-	// Transaction(driver.Tx) Repository
+	Transaction(driver.Tx) Repository
 }
