c6447658093da8106c81c6a4095149c0a060d6d2
antonio mello babo <antoniomelloaviao@gmail.com>
2021-05-10 17:54:35 -0300

    refactor: must be logged in to perform a transfer

--- app/gateways/transfer/transfer.go
+++ app/gateways/transfer/transfer.go
@@ -5,11 +5,21 @@ import (
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/mellotonio/desafiogo/app/domain/errors"
 	"github.com/mellotonio/desafiogo/app/gateways/http/response"
+	"github.com/mellotonio/desafiogo/app/infra/utils"
 )
 
 func (h TransferHandler) Transfer(w http.ResponseWriter, r *http.Request) {
-	accountOriginID := chi.URLParam(r, "accountID_origin")
+
+	// Must be logged in to perform a transaction
+	accDescription, err := utils.ExtractTokenMetadata(r)
+
+	if err != nil {
+		err := errors.ErrUnauthorized
+		response.Error(w, http.StatusUnauthorized, err)
+	}
+
 	accountDestinationID := chi.URLParam(r, "accountID_destination")
 	amount := chi.URLParam(r, "amount")
 
@@ -19,7 +29,7 @@ func (h TransferHandler) Transfer(w http.ResponseWriter, r *http.Request) {
 		response.Error(w, http.StatusInternalServerError, err)
 	}
 
-	transfer, err := h.service.Transfer(accountOriginID, accountDestinationID, amountInt)
+	transfer, err := h.service.Transfer(accDescription.AccountID, accountDestinationID, amountInt)
 
 	if err != nil {
 		response.Error(w, http.StatusInternalServerError, err)
