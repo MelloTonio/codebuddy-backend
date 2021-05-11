63560573017199acf152b760a04aeb44f0ab1316
MelloTonio <antoniombabo@uol.com.br>
2021-05-11 12:57:43 -0400

    test: showAllAccount transfers

---
+++ app/domain/transfer/usecases/ShowAllAccountTransfers_test.go
@@ -0,0 +1,21 @@
+package usecasesTransf
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func ShowAllAccountTransfersTest(t *testing.T) {
+	for _, v := range PassingStoreCases {
+		NewAccountService.CreateAccount(&v)
+	}
+
+	_, err := NewtransferenceService.Transfer(PassingStoreCases[0].Id, PassingStoreCases[1].Id, 2000)
+	assert.NoError(t, err)
+
+	_, err = NewAccountService.ShowAccounts()
+
+	assert.NoError(t, err)
+
+}
