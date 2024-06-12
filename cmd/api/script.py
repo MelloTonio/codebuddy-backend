18ec8712e09a73209fad75b8488cfb01225dd5f6
mellotonio <antoniombabo@uol.com.br>
2024-06-12 03:27:51 -0300

    refact: dont look at benicio secret key

--- cmd/api/script.py
+++ cmd/api/script.py
@@ -1,10 +1,4 @@
-# This program adds two numbers
-
-num1 = 1.5
-num2 = 6.3
-
-# Add two numbers
-sum = num1 + num2
-
-# Display the sum
-print('benicio foi pego na escada mamando ser desconhecido3')
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.221")
